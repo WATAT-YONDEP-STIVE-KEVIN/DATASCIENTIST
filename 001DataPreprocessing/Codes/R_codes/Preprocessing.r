87a1aa810a386364033f37ae4f7ab3f589eab42e
kevin <stivekevinwatatyondep@gmail.com>
2025-02-23 10:03:32 +0100

    23-02-2024 R-preprocessing

--- 001DataPreprocessing/Codes/R_codes/Preprocessing.r
+++ 001DataPreprocessing/Codes/R_codes/Preprocessing.r
@@ -1,8 +1,37 @@
 
 #Chargement du djeu de données
+data <- read.table('./../../Datasets/data.csv', header = TRUE,  dec = '.', sep = ',');
 
-data <- read.table('./../../Datasets/data.csv', header = TRUE,  dec = '.', sep = ',')
+#Affichage de quelques ligne du jeu de données
+cat("Quelques lignes du jeu de données\n")
+cat("\n");
+head(data);
 
-head(data)
+cat("\n");
+cat("Description du jeu de données\n");
+#Description du jeu de données
+cat("\n");
+summary(data);
 
-summary(data)
+cat("\n");
+
+
+missingValuesCountry <- sum(is.na(data$Country));
+missingValuesAge <- sum(is.na(data$Age));
+missingValuesSalary <- sum(is.na(data$Salary));
+missingValuesPurchased <- sum(is.na(data$Purchased));
+missingValuesRegion <- sum(is.na(data$Region));
+
+
+cat("Country missing values = ", missingValuesCountry, "\n");
+cat("Age missing values = ", missingValuesAge, "\n");
+cat("Purchased missing values = ", missingValuesPurchased, "\n");
+cat("Salary missing values = ", missingValuesSalary, "\n");
+cat("Region missing values = ", missingValuesRegion, "\n");
+
+#Remplacons les valeurs manquantes par la moyenne, on utiliera les colonnes dont on a compté au moins une valeurs manquante
+
+data$Age[is.na(data$Age)] <- mean(data$Age, na.rm=TRUE);
+data$Salary[is.na(data$Salary)] <- mean(data$Salary, na.rm=TRUE);
+
+cat(data$Region)
